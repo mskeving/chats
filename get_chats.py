9ac88a608af0bf1cef66469e37a94c3923bd2093
Melissa Skevington <melissa@hipmunk.com>
2016-10-07 12:06:58 -0700

    update db script

---
+++ get_chats.py
@@ -0,0 +1,109 @@
+import base64
+import json
+import re
+import sys
+
+from apiclient import errors
+
+import app
+from app.models import Chat
+from gmail_api import GmailApi
+
+service = GmailApi().get_service()
+
+# Sender comes in as "FirstName LastName <email@address.com>".
+# Keep the email address.
+email_regex = re.compile('<(.*?)>')
+
+db = app.db
+CHUNK_SIZE = 200
+
+
+def get_message_ids(q):
+
+    try:
+        response = service.users().messages().list(userId='me', q=q).execute()
+    except errors.HttpError as error:
+        print("An error occurred: {}".format(error))
+
+    messages = [msg for msg in response.get('messages')]
+
+    while response.get('nextPageToken'):
+        pt = response['nextPageToken']
+        try:
+            response = service.users().messages().list(userId='me',
+                                                       q=q,
+                                                       pageToken=pt,
+                                                       ).execute()
+        except errors.HttpError as error:
+            print("An error occurred: {}".format(error))
+            continue
+        messages.extend(response.get('messages'))
+
+    existing_messages = Chat.query.all()
+    existing_ids = [msg.message_id for msg in existing_messages]
+
+    message_ids = [
+        msg['id'] for msg in messages
+        if msg['id'] not in existing_ids
+    ]
+
+    return message_ids
+
+
+def commit_messages():
+    try:
+        db.session.commit()
+    except:
+        print("failed to commit some chats: ", sys.exc_info()[0])
+
+
+def add_chats(query):
+    message_ids = get_message_ids(query)
+
+    for i, msg_id in enumerate(message_ids):
+        response = service.users().messages().get(userId='me',
+                                                  id=msg_id,
+                                                  format='full').execute()
+        data = json.dumps(response)
+        thread_id = response['thread_id']
+        encoded_text = response['payload']['body']['data'].encode('utf-8')
+        text = base64.urlsafe_b64decode(encoded_text)
+        send_time = response['internalDate']
+
+        for header in response['payload']['headers']:
+            if header['name'] == "From":
+                try:
+                    sender = header['value']
+                    email_address = email_regex.findall(sender)[0].lower()
+                except:
+                    raise("Couldn't get email from {}".format(sender))
+
+        new_chat = Chat(
+            message_id=msg_id,
+            data=data,
+            thread_id=thread_id,
+            text=text,
+            send_time=send_time,
+            sender=email_address,
+        )
+        db.session.add(new_chat)
+
+        if i > 0 and i % CHUNK_SIZE == 0:
+            # commit by chunk size so you don't have to start over
+            commit_messages()
+            print("committed {}, {} to go".format(i, len(message_ids)-i))
+
+    # commit any extras
+    commit_messages()
+    print("finished query. {} new chats.".format(len(message_ids)))
+
+
+if __name__ == "__main__":
+    queries = [
+        "to:me from:philrha@gmail.com after:2016/01/01 label:chats",
+        "from:me to:philrha@gmail.com after:2016/01/01 label:chats",
+    ]
+
+    for q in queries:
+        add_chats(q)
